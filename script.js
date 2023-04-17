7eaadb02041456ae9867d646ca0453b1e16538e3
Monica Gomes <gomescsmonica@gmail.com>
2023-04-17 00:03:32 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+const btJogador1 = document.getElementById('btJogador1');
+const btJogador2 = document.getElementById('btJogador2');
+const btReiniciar = document.getElementById('btReiniciar');
+const resultado = document.getElementById('resultado');
+
+let Jogador1 = 0;
+let Jogador2 = 0;
+let jogadaAtual = 1;
+let rodadasJogadas = 0;
+
+function rodarDado() {
+    const sorteio = Math.floor(Math.random() * 6) + 1;
+    resultado.innerText = `Jogador ${jogadaAtual}: ${sorteio}`;
+
+    if (jogadaAtual === 1) {
+        Jogador1 += sorteio;
+        jogadaAtual = 2;
+    }
+    else {
+        Jogador2 += sorteio;
+        jogadaAtual = 1;
+        rodadasJogadas++;
+    }
+
+    if (rodadasJogadas === 10) {
+        if (Jogador1 > Jogador2) {
+            resultado.innerText += "\nJogador 1: Você Venceu!";
+        }
+        else if (Jogador2 > Jogador1) {
+            resultado.innerText += "\nJogador 2: Você Venceu!";
+        }
+        else {
+            resultado.innerText += "\nEmpate!";
+        }
+
+        btJogador1.disabled = true;
+        btJogador2.disabled = true;
+    }
+}
+
+btJogador1.onclick = rodarDado;
+btJogador2.onclick = rodarDado;
+btReiniciar.onclick = function () {
+    Jogador1 = 0;
+    Jogador2 = 0;
+    jogadaAtual = 1;
+    rodadasJogadas = 0;
+    resultado.innerText = "";
+    btJogador1.disabled = false;
+    btJogador2.disabled = false;
+}
