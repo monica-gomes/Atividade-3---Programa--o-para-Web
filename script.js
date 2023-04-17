65a240fecc27f864fd919c38af2aaab7c3687979
monica-gomes <gomescsmonica@gmail.com>
2023-04-17 20:09:11 -0300

    Adição de Novas Funcionalidades

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const btJogador1 = document.getElementById('btJogador1');
 const btJogador2 = document.getElementById('btJogador2');
 const btReiniciar = document.getElementById('btReiniciar');
 const resultado = document.getElementById('resultado');
+const numRodada = document.getElementById('numeroRodada');
 
 let Jogador1 = 0;
 let Jogador2 = 0;
 let jogadaAtual = 1;
 let rodadasJogadas = 0;
+let numeroRodada = 0;
 
 function rodarDado() {
     const sorteio = Math.floor(Math.random() * 6) + 1;
@@ -15,12 +17,20 @@ function rodarDado() {
     if (jogadaAtual === 1) {
         Jogador1 += sorteio;
         jogadaAtual = 2;
+        
+        btJogador1.disabled = true;
+        btJogador2.disabled = false;
     }
     else {
         Jogador2 += sorteio;
         jogadaAtual = 1;
         rodadasJogadas++;
+        numRodada.innerHTML = rodadasJogadas;
+
+        btJogador1.disabled = false;
+        btJogador2.disabled = true;
     }
+
 
     if (rodadasJogadas === 10) {
         if (Jogador1 > Jogador2) {
@@ -45,6 +55,7 @@ btReiniciar.onclick = function () {
     Jogador2 = 0;
     jogadaAtual = 1;
     rodadasJogadas = 0;
+    numRodada.innerHTML = 0;
     resultado.innerText = "";
     btJogador1.disabled = false;
     btJogador2.disabled = false;
